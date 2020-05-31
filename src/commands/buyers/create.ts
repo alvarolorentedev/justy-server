522e7b52fbaaaf3c595c7898640d1739555f8f75
kanekotic <me@kanekotic.com>
2020-05-31 16:19:49 +0200

    #1 | Alvaro | fix missing refactored file

--- src/commands/buyers/create.ts
+++ src/commands/buyers/create.ts
@@ -1,8 +1,8 @@
-import sqlDatabase from "../../dataSources/SqlDatabase"
+import sqlBuyerDatabase from "../../dataSources/SqlBuyerDatabase"
 import logger from '../../utils/logger'
 import { SimpleResponse } from '../../types/simpleResponse'
 
-export default async function createBuyer(dataSource: sqlDatabase, email: string, password: string, isTestRequest: boolean): Promise<SimpleResponse> {
+export default async function createBuyer(dataSource: sqlBuyerDatabase, email: string, password: string, isTestRequest: boolean): Promise<SimpleResponse> {
     try {
         await dataSource.createBuyer(email, password, isTestRequest)
         return { success: true }
