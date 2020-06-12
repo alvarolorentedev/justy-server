20fecd308d7606488c37cbc3f137440512e37bfc
kanekotic <me@kanekotic.com>
2020-06-12 09:31:20 +0200

    NOISSUE | Alvaro | fix lint

--- src/types/DataSources.ts
+++ src/types/DataSources.ts
@@ -3,6 +3,6 @@ import BuyerSqlDatabase from '../dataSources/buyer/SqlDatabase';
 import BidderSqlDatabase from '../dataSources/bidder/SqlDatabase';
 
 export type DataSources = {
-  sqlBuyerAPI: DataSource | BuyerSqlDatabase;
-  sqlBidderAPI: DataSource | BidderSqlDatabase;
+  sqlBuyerAPI: DataSource & BuyerSqlDatabase;
+  sqlBidderAPI: DataSource & BidderSqlDatabase;
 };
