7ed0a2fa570a5af8e97d4696dafd09d4d438f4bb
kanekotic <me@kanekotic.com>
2020-06-12 10:37:11 +0200

    NOISSUE | Alvaro | fix lint

--- src/types/BidderResolver.ts
+++ src/types/BidderResolver.ts
@@ -3,8 +3,8 @@ import { IResolverObject } from 'apollo-server-express';
 export type BidderResolver = {
   Query: Record<string, unknown>;
   Mutation: {
-    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-    createBidder: IResolverObject<any, any, any>;
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types */
+    createBidder: IResolverObject<any, any, any> & Function;
   };
 };
 
